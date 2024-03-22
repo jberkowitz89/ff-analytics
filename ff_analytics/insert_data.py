8634d584fbf3fed21be335691c7fcae15f5ee77e
Joey Berkowitz <joey.berkowitz@vercel.com>
2024-03-21 22:25:53 -0500

    inserting data

---
+++ ff_analytics/insert_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Define the database connection parameters
+db_params = {
+    'host': '127.0.0.1',
+    'database': 'ff_analytics',
+    'user': 'jberkowitz',
+    'password': 'password'
+}
+
+engine = create_engine(f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}/{db_params["database"]}')
+
+files = {'box_scores': 'data/box_scores.csv',
+         'settings': 'data/settings.csv',
+         'matchups': 'data/matchups.csv',
+         'players': 'data/players.csv',
+         'draft': 'data/draft.csv',
+         'teams': 'data/teams.csv'}
+
+if __name__ == '__main__':
+    for table_name, file_name in files.items():
+        df = pd.read_csv(file_name)
+        df.to_sql(table_name, engine, if_exists='replace', index=False, schema='raw')
